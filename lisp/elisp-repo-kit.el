31280cdcec55387efbfd4c2abc530481d3076b80
Psionik K <73710933+psionic-k@users.noreply.github.com>
2022-06-09 22:05:46 -0500

    Added email
    Removed some cruft
    
    Fixed several bugs in renaming.
    
    Removed unused files from testing.
    
    Signed-off-by: Psionik K <73710933+psionic-k@users.noreply.github.com>

--- lisp/elisp-repo-kit.el
+++ lisp/elisp-repo-kit.el
@@ -84,11 +84,12 @@ are renaming.  Existing files will be clobbered."
             (message "Used lexical variables %s %s" hard-replace rename-map)))
         elisp-repo-kit--rename-maps))
 
-(defun elisp-repo-kit--replace-strings (dir package-name author user-org)
+(defun elisp-repo-kit--replace-strings (dir package-name author user-org email)
   "Replace values in files that need renaming or re-licensing.
 DIR is where we are reaplacing.  PACKAGE-NAME is the new package.
 AUTHOR will be used in copyright notices.  USER-ORG will be used
-as the first part of the new github path."
+as the first part of the new github path.  EMAIL is shown after AUTHOR in
+package headers."
   (let ((default-directory dir)
         (github-path (concat user-org "/" package-name)))
     (mapc
@@ -102,10 +103,10 @@ as the first part of the new github path."
            (insert (concat ", " author)))
          (goto-char (point-min))
          (when (re-search-forward "<author>" nil t)
-           (replace-match author))
+           (replace-match (concat author ", <" email ">"))
          (goto-char (point-min))
          ;; replace license with GPL3 notice
-         (when (re-search-forward ";; Copyright \\(.\\|\n\\)*SOFTWARE.$" nil t)
+         (when (re-search-forward ";; Permission \\(.\\|\n\\)*SOFTWARE.$" nil t)
            (replace-match elisp-repo-kit--gpl3-notice))
          (goto-char (point-min))
          ;; update github paths for README links
@@ -115,7 +116,11 @@ as the first part of the new github path."
          ;; update remaining package name strings
          (while (re-search-forward "elisp-repo-kit" nil t)
            (replace-match package-name))
-         (save-buffer)))
+         (while (re-search-forward "Elisp Repo Kit" nil t)
+           (replace-match (string-join
+                           (mapcar 'capitalize (split-string "-" package-name))) " "))
+         (save-buffer 0)
+         (kill-buffer))))
      elisp-repo-kit--files-with-strings)))
 
 (defun elisp-repo-kit--dash-dep ()
@@ -153,14 +158,14 @@ and for forking as a new template repository."
     (error "Could not find git executible")))
 
 ;;;###autoload
-(defun elisp-repo-kit-rename-relicense (clone-dir package-name author user-org)
-  "Commentary:
-
+(defun elisp-repo-kit-rename-relicense (clone-dir package-name author user-org email)
+  "Rename and relicense your clone of elisp-repo-kit.
 CLONE-DIR is your elisp-repo-clone root.  PACKAGE-NAME should be
 the long name of the package, what will show up in melpa etc.
 AUTHOR will be used in copyright notices.  USER-ORG is either
 your user or organization, which forms the first part of a github
-repo path.
+repo path.  EMAIL is shown after AUTHOR in
+package headers.
 
 This command replaces all instances of:
 
@@ -175,30 +180,30 @@ Then renames the files to reflect package name
 Finally, MIT licenses are swapped with GPL3 license notices.
 Re-licensing is fully permitted by the MIT license and intended
 by the author of this repository."
-  (interactive "DCloned directory: \nsPackage name: \nsAuthor: \nsGithub organization or username: ")
+  (interactive "DCloned directory: \nsPackage name: \nsAuthor: \nsGithub organization or username: \nsEmail: ")
   (elisp-repo-kit--replace-strings
-   clone-dir package-name author user-org)
+   clone-dir package-name author user-org email)
   (elisp-repo-kit--rename-package
    clone-dir elisp-repo-kit--package-name package-name))
 
 ;;;###autoload
-(defun elisp-repo-kit-new (clone-dir package-name author user-org &optional rev)
+(defun elisp-repo-kit-new (clone-dir package-name author user-org email &optional rev)
   "Clone elisp-repo-kit, rename, and relicense in one step.
-
 CLONE-DIR is where you want to put your new package root.
 PACKAGE-NAME should be the long name of the package, what will
 show up in melpa etc.  AUTHOR will be used in copyright notices.
 USER-ORG is either your user or organization, which forms the
-first part of a github repo path.  Optional REV is either a tag,
-branch or revision used in git checkout.
+first part of a github repo path.  EMAIL is shown after AUTHOR in
+package headers.  Optional REV is either a tag, branch or
+revision used in git checkout.
 
 See comments in `elisp-repo-kit-clone' and
 `elisp-repo-kit-rename-relicense' for implementation information
 and more details about argument usage.."
-  (interactive "sPackage name: \nsAuthor: \nsGithub organization or username: \nsRev tag, or branch: ")
+  (interactive "sPackage name: \nsAuthor: \nsGithub organization or username: \nsEmail: \nsRev tag, or branch: ")
   (elisp-repo-kit-rename-relicense
    (elisp-repo-kit-clone clone-dir rev)
-   package-name author user-org))
+   package-name author user-org email))
 
 (provide 'elisp-repo-kit)
 ;;; elisp-repo-kit.el ends here
